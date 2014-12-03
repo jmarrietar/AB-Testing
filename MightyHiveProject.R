011b41224822dfb118dfb39faf493f63d1761166
jmarrietar <jmarrietar@gmail.com>
2014-12-03 15:31:26 -0500

    data

---
+++ MightyHiveProject.R
@@ -0,0 +1,10 @@
+#MightyHive is an advertising technology company that uses 
+#retargeting methods to send ads to users online.
+
+#Call Center Remarketing, -> retarget those consumers
+#online whom did not make a purchase.
+
+setwd("C:/Users/Alfonso/Desktop/JOM/AB-Testing")
+
+Abandoned_Data<-read.csv("Abandoned_Data_Seed.csv",header=TRUE)
+Reservation_Data<-read.csv("Reservation_Data_Seed.csv",header=T)
